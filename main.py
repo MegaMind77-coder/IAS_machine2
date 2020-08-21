2e98321566d779e50976982a234b988fc7c8faa9
MegaMind77-coder <shanware.ishan@gmail.com>
2020-08-22 01:24:57 +0530

    imporoved representation

--- main.py
+++ main.py
@@ -2,18 +2,42 @@ import bintodec
 import fillmemory
 import dectobin
 
-class MAR():
-    address = ''
-    pointer_value = 0
-    def __init__(self):
-        self.address = '000000000000'
-        self.pointer_value = 12
+def decode(ir, mar):
+    print("DECODE INSTRUCTION")
+    if(bintodec.bintodec(ir) == 1):
+        print("LOAD M({})".format(bintodec.bintodec(mar)))
+    elif(bintodec.bintodec(ir) == 2):
+        print("LOAD -M({})".format(bintodec.bintodec(mar)))
+    elif(bintodec.bintodec(ir) == 5):
+        print("ADD M({})".format(bintodec.bintodec(mar)))
+    elif(bintodec.bintodec(ir) == 33):
+        print("STOR M({})".format(bintodec.bintodec(mar)))
+    elif(bintodec.bintodec(ir) == 6):
+        print("SUB M({})".format(bintodec.bintodec(mar)))
+    elif(bintodec.bintodec(ir) == 13):
+        print("JUMP M({}, 0:19)".format(bintodec.bintodec(mar)))
+    elif(bintodec.bintodec(ir) == 14):
+        print("JUMP M({}, 20:39)".format(bintodec.bintodec(mar)))
+    elif(bintodec.bintodec(ir) == 15):
+        print("JUMP +M({}, 0:19)".format(bintodec.bintodec(mar)))
+    elif(bintodec.bintodec(ir) == 16):
+        print("JUMP +M({}, 20:39)".format(bintodec.bintodec(mar)))
+    elif(bintodec.bintodec(ir) == 255):
+        print("HALT")
 
-def showchanges(ac, mbr, ibr, ir, mar, pc):
+    print()
+
+def fetchchanges(ac, mq, mbr, ibr, ir, mar, pc):
+    print("FETCHING INSTRUCTION")
     if(ac == ''):
         print("AC = ",ac)
     else:
         print("AC = ",ac, "Decimal value of AC = ", bintodec.signedbintodec(bintodec.bintodec(ac), 40))
+
+    if(mq == ''):
+        print("MQ = ")
+    else:
+        print("MQ = ",mq, "Decimal value of MQ = ", bintodec.signedbintodec(bintodec.bintodec(mq), 40))
     print("MBR = ", mbr)
     print("IBR = ", ibr)
     if(ir == ''):
@@ -28,6 +52,30 @@ def showchanges(ac, mbr, ibr, ir, mar, pc):
     print("PC = ", pc+1)
     print()
 
+def executechanges(ac, mq, mbr, ibr, ir, mar, pc):
+    print("EXECUTE CHANGES")
+    if(ac == ''):
+        print("AC = ",ac)
+    else:
+        print("AC = ",ac, "Decimal value of AC = ", bintodec.signedbintodec(bintodec.bintodec(ac), 40))
+
+    if(mq == ''):
+        print("MQ = ")
+    else:
+        print("MQ = ",mq, "Decimal value of MQ = ", bintodec.signedbintodec(bintodec.bintodec(mq), 40))
+    print("MBR = ", mbr)
+    print("IBR = ", ibr)
+    if(ir == ''):
+        print("IR = ", ir)
+    else:
+        print("IR = ", ir, "Decimal value of IR = ", bintodec.bintodec(ir))
+    
+    if(mar == ''):
+        print("MAR = ", mar)
+    else:
+        print("MAR = ", mar, "Decimal value of MAR = ", bintodec.bintodec(mar))
+    print("PC = ", pc+1)
+    print()
 
 if __name__ == "__main__":
     
@@ -60,48 +108,81 @@ if __name__ == "__main__":
             mar = ibr[8:20]
             pc = pc + 1
             ibr = ''
-        showchanges(ac, mbr, ibr, ir, mar, pc)
+
+        fetchchanges(ac, mq, mbr, ibr, ir, mar, pc)
+
+        decode(ir,mar)
         
         if(bintodec.bintodec(ir) == 1):            #LOAD M(X) INSTRUCTION
             mbr = memory[bintodec.bintodec(mar) - 1]
             ac = mbr
+            executechanges(ac, mq, mbr, ibr, ir, mar, pc)
+
+        elif(bintodec.bintodec(ir) == 2):         #LOAD -M(X) INSTRUCTION
+            mbr = memory[bintodec.bintodec(mar) - 1]
+            ac = dectobin.dectosignedbin(-1*bintodec.signedbintodec(bintodec.bintodec(mbr), 40))
+            executechanges(ac, mq, mbr, ibr, ir, mar, pc)
 
         elif(bintodec.bintodec(ir) == 5):          #ADD M(X) INSTRUCTION
             mbr = memory[bintodec.bintodec(mar) - 1]
             mbr_dec = bintodec.signedbintodec(bintodec.bintodec(mbr), 40)
             ac = dectobin.dectosignedbin(bintodec.signedbintodec(bintodec.bintodec(ac), 40) + mbr_dec)
+            executechanges(ac, mq, mbr, ibr, ir, mar, pc)
+
 
         elif(bintodec.bintodec(ir) == 33):    #STOR M(X) INSTRUCTION
             memory[bintodec.bintodec(mar) - 1] = ac
             change_location = bintodec.bintodec(mar)
             print('Memory at {} changed to {}'.format(change_location, memory[bintodec.bintodec(mar) - 1]))
             print()
+            executechanges(ac, mq, mbr, ibr, ir, mar, pc)
+
 
         elif(bintodec.bintodec(ir) == 6):    #SUB M(X) INSTRUCTION
             mbr = memory[bintodec.bintodec(mar) - 1]
             mbr_dec = bintodec.signedbintodec(bintodec.bintodec(mbr), 40)
             ac = dectobin.dectosignedbin(bintodec.signedbintodec(bintodec.bintodec(ac), 40) - mbr_dec)
+            executechanges(ac, mq, mbr, ibr, ir, mar, pc)
+
 
         elif(bintodec.bintodec(ir) == 15):   #JUMP +M(X, 0:19) INSTRUCTION
             ac_dec = bintodec.signedbintodec(bintodec.bintodec(ac), 40)
             if(ac_dec > 0):
                 pc = bintodec.bintodec(mar) - 1
                 ibr = ''
+            executechanges(ac, mq, mbr, ibr, ir, mar, pc)
 
-        elif(bintodec.bintodec(ir) == 14):  #JUMP +M(X, 20:39) INSTRUCTION
+        elif(bintodec.bintodec(ir) == 16):  #JUMP +M(X, 20:39) INSTRUCTION
             ac_dec = bintodec.signedbintodec(bintodec.bintodec(ac), 40)
             if(ac_dec > 0):
                 pc = bintodec.bintodec(mar) - 1
                 ibr = ''
                 mbr = memory[bintodec.bintodec(mar) - 1]
                 ibr = mbr[20:40]
-                print(ibr)
+            executechanges(ac, mq, mbr, ibr, ir, mar, pc)
+
+                
+        elif(bintodec.bintodec(ir) == 13):  #JUMP M(X, 0:19) INSTRUCTION
+            pc = bintodec.bintodec(mar) - 1
+            ibr = ''
+            executechanges(ac, mq, mbr, ibr, ir, mar, pc)
+
+        
+        elif(bintodec.bintodec(ir) == 14):  #JUMP M(X, 20:39) INSTRUCTION
+            pc = bintodec.bintodec(mar) - 1
+            ibr = ''
+            mbr = memory[bintodec.bintodec(mar) - 1]
+            ibr = mbr[20:40]
+            executechanges(ac, mq, mbr, ibr, ir, mar, pc)
+
 
         elif(bintodec.bintodec(ir) == 255):   #HALT INSTRUCTION
             execution = False
             file3 = open("out.txt", "w")
             for x in memory:
                file3.write(x+'\n')
+        
+        
             
             
         
